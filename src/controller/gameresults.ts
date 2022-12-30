7bb007a594904e874999d9d2290d0421bd5f910f
Grant Montgomery <lettucebowler@gmail.com>
2022-12-29 22:48:54 -0600

    update to grab user id

--- src/controller/gameresults.ts
+++ src/controller/gameresults.ts
@@ -33,7 +33,7 @@ export const validateGetInfoForLeaderboardRequest = validator((v) => ({
 export const getInfoForLeaderboard: MiddlewareHandler = async (c) => {
 	const gamenum = parseInt(`${c.req.valid().gamenum}`);
 	const query = c.env.WORDLETTUCE_DB.prepare(
-		`SELECT USERNAME, SUM(ATTEMPTS), COUNT(ATTEMPTS), (COUNT(ATTEMPTS) * 7) - SUM(ATTEMPTS) FROM game_results a inner join users b on a.user_id = b.github_id WHERE GAMENUM > ?1 AND GAMENUM <= ?2 GROUP BY USER_id ORDER BY (COUNT(ATTEMPTS) * 7) - SUM(ATTEMPTS) DESC LIMIT 10`
+		`SELECT USERNAME, GITHUB_ID, SUM(ATTEMPTS), COUNT(ATTEMPTS), (COUNT(ATTEMPTS) * 7) - SUM(ATTEMPTS) FROM game_results a inner join users b on a.user_id = b.github_id WHERE GAMENUM > ?1 AND GAMENUM <= ?2 GROUP BY USER_id ORDER BY (COUNT(ATTEMPTS) * 7) - SUM(ATTEMPTS) DESC LIMIT 10`
 	).bind(gamenum - 7, gamenum);
 	const { success, results } = await query.all();
 
@@ -42,9 +42,10 @@ export const getInfoForLeaderboard: MiddlewareHandler = async (c) => {
 	}
 
 	const mapped = results.map((row) => {
-		const [user, sum, count, score] = Object.values(row);
+		const [user, userId, sum, count, score] = Object.values(row);
 		return {
 			user,
+			userId,
 			sum,
 			count,
 			score
@@ -64,7 +65,7 @@ export const saveGameResults: MiddlewareHandler = async (c) => {
 	const attempts = Math.floor(answers.toString().length / 5);
 	const query = c.env.WORDLETTUCE_DB.prepare(
 		'INSERT INTO GAME_RESULTS (GAMENUM, USER_ID, ANSWERS, ATTEMPTS) VALUES (?1, ?2, ?3, ?4) ON CONFLICT (USER_id, GAMENUM) DO UPDATE SET ANSWERS=?5, ATTEMPTS=?6'
-	).bind(gamenum, user, answers, attempts, answers, attempts);
+	).bind(gamenum, user, answers.slice(-30), attempts, answers.slice(-30), attempts);
 	const results = await query.run();
 	console.log('results', results);
 
