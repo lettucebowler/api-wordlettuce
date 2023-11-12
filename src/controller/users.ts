fb58fd3ab20386100ab0f52503ef00cb8e5b69c2
Grant Montgomery <lettucebowler@gmail.com>
2023-11-12 01:40:26 -0600

    remove deprecated endpoint

--- src/controller/users.ts
+++ src/controller/users.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono';
 import { object, coerce } from 'valibot';
 import { ApiWordLettuceBindings } from '../util/env';
 import { validateRequest } from '../util/validate';
-import { answerSchema, gameNumSchema, userIdSchema, usernameSchema } from '../util/schemas';
+import { userIdSchema, usernameSchema } from '../util/schemas';
 
 const userController = new Hono<{ Bindings: ApiWordLettuceBindings }>();
 
@@ -38,40 +38,6 @@ userController.put('/:userId', async (c) => {
 			username
 		},
 		meta
-	});
-});
-
-const createGameResultRequestSchema = object({
-	param: object({
-		userId: coerce(userIdSchema, Number),
-		gameNum: coerce(gameNumSchema, Number)
-	}),
-	json: object({
-		answers: answerSchema
-	})
-});
-userController.put(':userId/game-results/:gameNum', async (c) => {
-	const data = await validateRequest(createGameResultRequestSchema, c);
-	const { userId, gameNum } = data.param;
-	const { answers } = data.json;
-	const query = c.env.WORDLETTUCE_DB.prepare(
-		'INSERT INTO GAME_RESULTS (GAMENUM, USER_ID, ANSWERS, attempts) VALUES (?1, ?2, ?3, ?4) ON CONFLICT (USER_id, GAMENUM) DO UPDATE SET ANSWERS=?3, attempts=?4 returning gamenum gameNum, user_id userId, answers, attempts'
-	).bind(gameNum, userId, answers.slice(-30), answers.length / 5);
-	const createResult = await query.run();
-	if (!createResult.success) {
-		return c.json(
-			{
-				success: false,
-				data: createResult
-			},
-			500
-		);
-	}
-	const [created] = createResult.results;
-	return c.json({
-		success: true,
-		data: created,
-		meta: createResult.meta
 	});
 });
 
