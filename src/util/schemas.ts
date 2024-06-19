b557d5c35886faf7c350dd3110712ad4a0499bba
Grant Montgomery <lettucebowler@gmail.com>
2024-06-19 00:16:23 -0500

    bleh

--- src/util/schemas.ts
+++ src/util/schemas.ts
@@ -1,9 +1,11 @@
 import { custom, integer, minValue, number, regex, string } from 'valibot';
-export const positiveInteger = number([integer(), minValue(1)]);
+import * as v from 'valibot';
+export const positiveInteger = v.pipe(v.number(), v.integer(), v.minValue(1));
 export const userIdSchema = positiveInteger;
-export const usernameSchema = string();
+export const usernameSchema = v.string();
 export const gameNumSchema = positiveInteger;
-export const answerSchema = string([
-	regex(/[xci_]/),
-	custom((input) => input.length % 5 === 0, 'Must be multiple of 5 characters')
-]);
+export const answerSchema = v.pipe(
+	v.string(),
+	v.regex(/[xci_]/),
+	v.custom((input) => input.length % 5 === 0, 'Must be multiple of 5 characters')
+);
