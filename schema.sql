400a61bf83fb7164b74958014e198b6971fca059
Grant Montgomery <lettucebowler@gmail.com>
2024-06-20 02:10:12 -0500

    add user search endpoint by provider and provider id

--- schema.sql
+++ schema.sql
@@ -1,15 +1,30 @@
-drop table if exists users;
-CREATE TABLE `users` (
-	`id` INTEGER primary key AUTOINCREMENT,
-	`github_id` int NOT NULL,
-	`username` varchar(255)
-);
+-- drop table if exists users;
+-- CREATE TABLE `users` (
+-- 	`id` INTEGER primary key AUTOINCREMENT,
+-- 	`github_id` int NOT NULL,
+-- 	`username` varchar(255)
+-- );
 
-drop table if exists game_results;
-CREATE TABLE `game_results` (
-	`gamenum` int NOT NULL,
-	`answers` varchar(30) NOT NULL,
-	`user_id` int NOT NULL,
-	`attempts` int NOT NULL,
-	PRIMARY KEY (`gamenum`, `user_id`)
+-- drop table if exists game_results;
+-- CREATE TABLE `game_results` (
+-- 	`gamenum` int NOT NULL,
+-- 	`answers` varchar(30) NOT NULL,
+-- 	`user_id` int NOT NULL,
+-- 	`attempts` int NOT NULL,
+-- 	PRIMARY KEY (`gamenum`, `user_id`)
+-- );
+
+-- drop table if exists identities;
+-- CREATE TABLE `identities` (
+-- 	`user_id` int not null,
+-- 	`provider` text not null,
+-- 	`provider_id` text not null,
+-- 	primary key (`user_id`, `provider`)
+-- );
+
+drop table if exists personas;
+create table `personas` (
+	`id` INTEGER primary key AUTOINCREMENT,
+	`username` text not null,
+	`email` text
 );
--- src/controller/users.ts
+++ src/controller/users.ts
@@ -3,6 +3,7 @@ import * as v from 'valibot';
 import { ApiWordLettuceBindings } from '../util/env';
 import { UserIdSchema, UsernameSchema } from '../util/schemas';
 import { vValidator } from '@hono/valibot-validator';
+import { IdentitySchema } from '../schemas';
 
 const userController = new Hono<{ Bindings: ApiWordLettuceBindings }>();
 
@@ -43,5 +44,57 @@ userController.put(
 		});
 	}
 );
+
+const getUsersQuerySchema = v.object({
+	provider: v.string(),
+	providerId: v.string()
+});
+function mapIdentitiesToUser(identities: Array<v.InferOutput<typeof IdentitySchema>>) {
+	const [first] = identities;
+	if (!first) {
+		throw new Error('Oh no!');
+	}
+
+	let user: {
+		id: number;
+		username: string;
+		email: string;
+		identities: Array<{ provider: string; id: string }>;
+	} = {
+		id: first.personaId,
+		username: first.username,
+		email: first.email,
+		identities: identities.map((identity) => {
+			return {
+				provider: identity.provider,
+				id: identity.providerId
+			};
+		})
+	};
+	return user;
+}
+userController.get('', vValidator('query', getUsersQuerySchema), async (c) => {
+	const { provider, providerId } = c.req.valid('query');
+	const query = c.env.WORDLETTUCE_DB.prepare(
+		'select id as persona_id, username, email, provider, provider_id from personas p inner join identities i on p.id = i.persona_id where provider = ?1 and provider_id = ?2'
+	).bind(provider, providerId);
+	const { results, meta } = await query.all();
+	const identities = v.safeParse(v.array(IdentitySchema), results);
+	if (!identities.success) {
+		return c.json(
+			{
+				message: 'Invalid data from db'
+			},
+			500
+		);
+	}
+	const users = Object.values(
+		Object.groupBy(identities.output, (identity) => identity.personaId)
+	).map((identities) => mapIdentitiesToUser(identities ?? []));
+	return c.json({
+		users,
+		meta
+	});
+});
 
 export default userController;
